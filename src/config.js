01803c8908888ae32d32b195934dc5ba144aba0c
Ben Grabowski <gbowski30@gmail.com>
2020-02-01 13:57:53 -0700

    adds .env

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
     // API_ENDPOINT: 'http://localhost:8000/api',
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL, 
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api', 
     TOKEN_KEY: 'planit-client-auth-token',
 };
